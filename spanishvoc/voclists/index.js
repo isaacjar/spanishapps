9c234de56b01c3af2d093bd6fc48af3e41db7cb3
isaacjar <isaacjaramago@gmail.com>
2025-08-29 19:28:35 +0200

    Update index.js

--- spanishvoc/voclists/index.js
+++ spanishvoc/voclists/index.js
@@ -1,7 +1,7 @@
 const voclists = [
   { filename: "a1_001_basic", title: "A1 001 Básico (32)", level: "A1", misc: "Isaac" },
-  { filename: "a1_002_verbs", title: "A1 002 Verbos (10)", level: "A1", misc: "Isaac" },
-  { filename: "a1_003_adjectives", title: "A1 003 Adjetivos (10)", level: "A1", misc: "Isaac" },
-  { filename: "a1_004_nouns2", title: "A1 004 Sustantivos (10)", level: "A1", misc: "Isaac" },
+  { filename: "a1_002_verbs", title: "A1 002 Verbos (25)", level: "A1", misc: "Isaac" },
+  { filename: "a1_003_adjectives", title: "A1 003 Adjetivos (25)", level: "A1", misc: "Isaac" },
+  { filename: "a1_004_nouns2", title: "A1 004 Sustantivos (25)", level: "A1", misc: "Isaac" },
   // más listados...
 ];
