8b0e61c63f6201cce1ba808ecc6b67a6a0798bb6
isaacjar <isaacjaramago@gmail.com>
2025-08-29 19:03:41 +0200

    Update index.js

--- spanishvoc/voclists/index.js
+++ spanishvoc/voclists/index.js
@@ -1,4 +1,4 @@
 const voclists = [
-  { filename: "A1001Basic", title: "A1 001 Básico (32)", level: "A1", misc: "Isaac" },
+  { filename: "a1_001_basic", title: "A1 001 Básico (32)", level: "A1", misc: "Isaac" },
   // más listados...
 ];
