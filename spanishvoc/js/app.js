c0218fd38e0731d8a7ce44a040773af84a23f9d8
isaacjar <isaacjaramago@gmail.com>
2025-08-31 17:51:46 +0200

    Añade fetch

--- spanishvoc/js/app.js
+++ spanishvoc/js/app.js
@@ -3,4 +3,18 @@ window.addEventListener("DOMContentLoaded", () => {
   UI.init();
   Game.init();
   Stats.init();
+
+  // cargar lista de listas
+  fetch("https://isaacjar.github.io/spanishapps/spanishvoc/voclists/index.js")
+    .then(r => r.text())
+    .then(code => {
+      eval(code); // crea la variable global voclists
+      UI.renderVoclists(voclists);
+
+      // Si hay voclist en URL → cargar directamente
+      const urlParams = new URLSearchParams(window.location.search);
+      if (urlParams.has("voclist")) {
+        UI.loadVoclist(urlParams.get("voclist"));
+      }
+    });
 });
