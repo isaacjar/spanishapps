57e196fc0b3e1022570d2e8a94cdc70f0d36f53b
isaacjar <isaacjaramago@gmail.com>
2025-09-01 15:28:26 +0200

    Update ui.js

--- spanishvoc/js/ui.js
+++ spanishvoc/js/ui.js
@@ -1,4 +1,7 @@
 import { voclists } from "./index.js";
+import { Game } from "./game.js";
+import { Stats } from "./stats.js";
+import { Settings } from "./settings.js";
 
 export const UI = {
   currentScreen: null,
