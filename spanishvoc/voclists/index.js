1660735bff0c40aad7199f435cc2b7477bf0c33e
Isaac JH <isaacjaramago@gmail.com>
2025-11-02 12:48:19 +0100

    Update index.js

--- spanishvoc/voclists/index.js
+++ spanishvoc/voclists/index.js
@@ -26,7 +26,7 @@ export const voclists = [
   { filename: "a1_023_numbers100", title: "A1 023 Números 1-100 (37)", level: "A1", misc: "Isaac" },
   { filename: "a1_024_daily_routine", title: "A1 024 Rutina diaria (25)", level: "A1", misc: "Isaac" },
   { filename: "a1_025_moments", title: "A1 025 Momentos (25)", level: "A1", misc: "Isaac" },
-  { filename: "a1_026_school", title: "A1 026 La escuela (25)", level: "A1", misc: "Isaac" },
+  { filename: "a1_026_school", title: "A1 026 La escuela (42)", level: "A1", misc: "Isaac" },
   { filename: "a1_027_formas", title: "A1 027 Formas (25)", level: "A1", misc: "Isaac" },
   { filename: "a1_028_emotions", title: "A1 028 Emociones (25)", level: "A1", misc: "Isaac" },
   { filename: "a1_029_city", title: "A1 029 La ciudad (25)", level: "A1", misc: "Isaac" },
