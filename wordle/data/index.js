8ed1cd71c1b76c866f9403a3cbff377ea1f6fde1
Isaac JH <isaacjaramago@gmail.com>
2025-12-24 10:04:58 +0100

    Create index.js

---
+++ wordle/data/index.js
@@ -0,0 +1,4 @@
+const voclists = [
+  { filename: "words_es_L1", title: "Spanish Level 1", level: "SA1", misc: "" },
+  { filename: "words_en_L1.json", title: "English Level 1", level: "EA1", misc: "" }
+]
