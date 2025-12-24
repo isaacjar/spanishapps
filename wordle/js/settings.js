479635bcea628137813c699f3d41ce5d9947c474
Isaac JH <isaacjaramago@gmail.com>
2025-12-24 20:58:41 +0100

    Update settings.js

--- wordle/js/settings.js
+++ wordle/js/settings.js
@@ -1,17 +1,27 @@
 const Settings = {
   defaults: { lang: "es", numint: 6 },
 
+  /* =========================
+     CARGA SETTINGS
+  ========================= */
   load() {
     const url = new URLSearchParams(location.search);
-    return {
-      lang: url.get("lang") || localStorage.lang || this.defaults.lang,
-      numint: parseInt(url.get("numint")) || localStorage.numint || this.defaults.numint,
-      voclist: url.get("voclist")
-    };
+    let lang = url.get("lang") || localStorage.lang || this.defaults.lang;
+    let numint = parseInt(url.get("numint")) || parseInt(localStorage.numint) || this.defaults.numint;
+    let voclist = url.get("voclist") || null;
+
+    // Validaciones
+    if (!["es","en"].includes(lang)) lang = this.defaults.lang;
+    if (numint < 4 || numint > 10) numint = this.defaults.numint;
+
+    return { lang, numint, voclist };
   },
 
+  /* =========================
+     GUARDAR SETTINGS
+  ========================= */
   save(s) {
-    localStorage.lang = s.lang;
-    localStorage.numint = s.numint;
+    if (s.lang) localStorage.lang = s.lang;
+    if (s.numint) localStorage.numint = s.numint;
   }
 };
