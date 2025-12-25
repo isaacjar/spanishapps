9734162ac17b81d9387eb932622bccb70e2849d9
Isaac JH <isaacjaramago@gmail.com>
2025-12-25 11:19:26 +0100

    Create check_5_en.js

---
+++ wordle/data/check_5_en.js
@@ -0,0 +1,14 @@
+export default [
+"about","above","after","again","apple","begin","black","brain","bread","break",
+"bring","brown","build","chair","child","clock","cloud","coach","color","could",
+"daily","dance","dream","drink","earth","email","every","faith","field","first",
+"flour","flower","front","fruit","glass","glove","grass","great","green","group",
+"happy","heart","house","human","juice","knife","laugh","learn","light","lunch",
+"money","month","morning","mouse","mouth","music","never","night","north","ocean",
+"often","paint","paper","party","piano","pizza","place","plant","plate","power",
+"price","quiet","radio","rainy","ready","right","river","round","salad","scale",
+"school","share","shirt","short","sight","sleep","small","smile","snowy","solar",
+"sound","south","space","speak","spend","sport","spring","stand","stone","story",
+"sugar","sunny","table","thank","thing","think","today","train","under","until",
+"water","watch","wheel","where","which","woman","world","write","wrong","young"
+];
