f3475d039a0d2bdcb4d6aa34bd1a1f0f9bfa9bf5
Isaac JH <isaacjaramago@gmail.com>
2025-12-25 11:04:05 +0100

    Update index.js

--- wordle/data/index.js
+++ wordle/data/index.js
@@ -3,3 +3,4 @@ const voclists = [
   { filename: "words_es_L2", val: "check_5_es", title: "Español 2 - 5 letras (481 palabras)", level: "SA1", num: "5" },
   { filename: "words_en_L1", val: "check_5_en", title: "English Level 1 (5 words)", level: "EA1", num: "5" }
 ]
+window.voclists = voclists;
