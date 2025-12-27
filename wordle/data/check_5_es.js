f1daac3db2571880675c84e125a5fb6088f8f748
Isaac JH <isaacjaramago@gmail.com>
2025-12-27 12:35:14 +0100

    Update check_5_es.js

--- wordle/data/check_5_es.js
+++ wordle/data/check_5_es.js
@@ -147,7 +147,7 @@ export default [
 "sanar", "sanes", "sanen", "solió", "súper", "tésis", "tibia", "tibio", "tilde", "tóner", "tórax", "verte", "verbo", "ictus", "pollo", "metas", "ceras", "telón", "cresa", "tersa",
 "lelos", "lelas", "tieso", "tiesa", "parca", "ramón", "remas", "reman", "caber", "cabía", "mirra", "bollo", "codos", "podón", "dolor", "sabor", "nabos", "caleb", "perra", "gafas",
 "lobos", "locos", "locas", "lobas", "flora", "gallo", "cejas", "mando", "cinto", "cinta", "manda", "manga", "fideo", "oreja", "mango", "cacas", "cómic", "cagar", "cagan", "cagas",
-"caros", "choco"
+"caros", "choco", "tosas", "losas", "mojón", "molón", "copla", "mopas", "moras", "coros", "roían", "yerre", "yerra"
 ];
 
 
