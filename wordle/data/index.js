ca007c62d5d5944d2e6c240a2985fd726b30e103
Isaac JH <isaacjaramago@gmail.com>
2025-12-28 12:21:26 +0100

    Update index.js

--- wordle/data/index.js
+++ wordle/data/index.js
@@ -1,7 +1,8 @@
 // index.js
 const voclists = [
   { filename: "words_es_L1", val: "check_5_es", title: "Español 1 - 5 letras (351 palabras)", level: "SA1", num: "5" },
-  { filename: "words_es_L2", val: "check_5_es", title: "Español 2 - 5 letras (481 palabras)", level: "SA1", num: "5" },
+  { filename: "words_es_L2", val: "check_5_es", title: "Español 2 - 5 letras (481 palabras)", level: "SA2", num: "5" },
+  { filename: "words_es_L3", val: "check_5_es", title: "Español 3 - 5 letras (1000 palabras)", level: "SB1", num: "5" },
   { filename: "words_en_L1", val: "check_5_en", title: "English Level 1 (5 words)", level: "EA1", num: "5" }
 ]
 window.voclists = voclists;
